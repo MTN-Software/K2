dcd9689902bbeffb52d3e896a862af49eba0f303
Thomas Maloney <ThomasTNF@live.com>
2014-02-15 00:20:06 -0600

    new file:   HTMLImageMapper/HTMLImageMapper/HTMLImageMapper.sln 	new file:   HTMLImageMapper/HTMLImageMapper/HTMLImageMapper/App.config 	new file:   HTMLImageMapper/HTMLImageMapper/HTMLImageMapper/FormNamePrompt.Designer.vb 	new file:   HTMLImageMapper/HTMLImageMapper/HTMLImageMapper/FormNamePrompt.resx 	new file:   HTMLImageMapper/HTMLImageMapper/HTMLImageMapper/FormNamePrompt.vb 	new file:   HTMLImageMapper/HTMLImageMapper/HTMLImageMapper/ForumAbout.Designer.vb 	new file:   HTMLImageMapper/HTMLImageMapper/HTMLImageMapper/ForumAbout.resx 	new file:   HTMLImageMapper/HTMLImageMapper/HTMLImageMapper/ForumAbout.vb 	new file:   HTMLImageMapper/HTMLImageMapper/HTMLImageMapper/HTMLImageMapper.vbproj 	new file:   HTMLImageMapper/HTMLImageMapper/HTMLImageMapper/Images/Default.png 	new file:   HTMLImageMapper/HTMLImageMapper/HTMLImageMapper/Logo.png 	new file:   HTMLImageMapper/HTMLImageMapper/HTMLImageMapper/MTNSoftwareLogo.png 	new file:   HTMLImageMapper/HTMLImageMapper/HTMLImageMapper/MainForm.Designer.vb 	new file:   HTMLImageMapper/HTMLImageMapper/HTMLImageMapper/MainForm.resx 	new file:   HTMLImageMapper/HTMLImageMapper/HTMLImageMapper/MainForm.vb 	new file:   HTMLImageMapper/HTMLImageMapper/HTMLImageMapper/My Project/Application.Designer.vb 	new file:   HTMLImageMapper/HTMLImageMapper/HTMLImageMapper/My Project/Application.myapp 	new file:   HTMLImageMapper/HTMLImageMapper/HTMLImageMapper/My Project/AssemblyInfo.vb 	new file:   HTMLImageMapper/HTMLImageMapper/HTMLImageMapper/My Project/Resources.Designer.vb 	new file:   HTMLImageMapper/HTMLImageMapper/HTMLImageMapper/My Project/Resources.resx 	new file:   HTMLImageMapper/HTMLImageMapper/HTMLImageMapper/My Project/Settings.Designer.vb 	new file:   HTMLImageMapper/HTMLImageMapper/HTMLImageMapper/My Project/Settings.settings 	new file:   HTMLImageMapper/HTMLImageMapper/HTMLImageMapper/My Project/app.manifest 	new file:   HTMLImageMapper/HTMLImageMapper/HTMLImageMapper/Settings.vb 	new file:   HTMLImageMapper/HTMLImageMapper/HTMLImageMapper/SettingsForm.Designer.vb 	new file:   HTMLImageMapper/HTMLImageMapper/HTMLImageMapper/SettingsForm.resx 	new file:   HTMLImageMapper/HTMLImageMapper/HTMLImageMapper/SettingsForm.vb 	new file:   HTMLImageMapper/HTMLImageMapper/HTMLImageMapper/SplashScreen.Designer.vb 	new file:   HTMLImageMapper/HTMLImageMapper/HTMLImageMapper/SplashScreen.resx 	new file:   HTMLImageMapper/HTMLImageMapper/HTMLImageMapper/SplashScreen.vb 	new file:   HTMLImageMapper/HTMLImageMapper/HTMLImageMapper/SplashScreenImage.jpg

---
+++ HTMLImageMapper/HTMLImageMapper/HTMLImageMapper/FormNamePrompt.vb
@@ -0,0 +1,20 @@
+﻿Public Class frmNamePrompt
+
+    Private Sub txtHref_Click(sender As Object, e As EventArgs) Handles txtHref.Click
+        Dim ofdLink As OpenFileDialog = New OpenFileDialog()
+
+        ofdLink.Title = "MTN HTML Image Mapper - Open HTML File"
+        ofdLink.InitialDirectory = "C:\"
+        ofdLink.Filter = "All Files (*.*)|*.*| HTML (*.htm)|*.htm*"
+        ofdLink.FilterIndex = 2
+        ofdLink.RestoreDirectory = True
+
+        If ofdLink.ShowDialog() = Windows.Forms.DialogResult.OK Then
+            txtHref.Text = ofdLink.FileName
+        End If
+    End Sub
+
+    Private Sub btnOK_Click(sender As Object, e As EventArgs) Handles btnOK.Click
+        Me.Close()
+    End Sub
+End Class
---
+++ HTMLImageMapper/HTMLImageMapper/HTMLImageMapper/Settings.vb
@@ -0,0 +1,23 @@
+﻿
+Namespace My
+    
+    'This class allows you to handle specific events on the settings class:
+    ' The SettingChanging event is raised before a setting's value is changed.
+    ' The PropertyChanged event is raised after a setting's value is changed.
+    ' The SettingsLoaded event is raised after the setting values are loaded.
+    ' The SettingsSaving event is raised before the setting values are saved.
+    Partial Friend NotInheritable Class MySettings
+
+        Private Sub MySettings_PropertyChanged(sender As Object, e As System.ComponentModel.PropertyChangedEventArgs) Handles Me.PropertyChanged
+            REM TODO:
+        End Sub
+
+        Private Sub MySettings_SettingsLoaded(sender As Object, e As System.Configuration.SettingsLoadedEventArgs) Handles Me.SettingsLoaded
+            MySettings.Default.blnBetaParticipate = False
+            MySettings.Default.strFirstName = String.Empty
+            MySettings.Default.strLastName = String.Empty
+            MySettings.Default.strProductCode = String.Empty
+
+        End Sub
+    End Class
+End Namespace
