2e4ed74cc0592a697fd798ea5074d65d57156d2c
Thomas Maloney <ThomasTNF@live.com>
2014-02-16 23:47:27 -0600

    Added visual studio 2008 support to the application

---
+++ HTMLImageMapper/2008Support/HTMLImageMapper/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("MTN Soft. HTML Image Mapper")> 
+<Assembly: AssemblyDescription("New from MTN Software, an HTML Image Mapper for Windows!  As always, it is open source and hosted on our github page.  Complaints, suggestions, and bug reports can go here -->mtnsoft.Spam@live.com")> 
+<Assembly: AssemblyCompany("MTN Software")> 
+<Assembly: AssemblyProduct("MTN HTML Image Mapper")> 
+<Assembly: AssemblyCopyright("Copyright ©  2014")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("05e97230-f316-489f-9394-7e8623079636")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.2.0.0")> 
+<Assembly: AssemblyFileVersion("1.2.0.4")> 
