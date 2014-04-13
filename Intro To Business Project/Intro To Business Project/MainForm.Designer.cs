b82b8276df0fe0931b01725a7a136366acef2541
Thomas Maloney <thomastnf@live.com>
2014-04-13 17:03:15 -0500

    re-added title

--- Intro To Business Project/Intro To Business Project/MainForm.Designer.cs
+++ Intro To Business Project/Intro To Business Project/MainForm.Designer.cs
@@ -693,7 +693,7 @@
             this.MainMenuStrip = this.mnuStrip;
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "frmMain";
-            this.Text = "               ";
+            this.Text = "Web Studio        ";
             this.Load += new System.EventHandler(this.frmMain_Load);
             this.mnuStrip.ResumeLayout(false);
             this.mnuStrip.PerformLayout();
